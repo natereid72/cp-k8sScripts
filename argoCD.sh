255d0d1a4a846e0e80376f8d4645ce70a0eb1a79
Nate Reid <45366367+natereid72@users.noreply.github.com>
2022-03-07 16:48:07 -0500

    Update argoCD.sh

--- argoCD.sh
+++ argoCD.sh
@@ -24,4 +24,3 @@ kubectl -n argocd patch secret argocd-secret \
     "admin.passwordMtime": "'$(date +%FT%T%Z)'"}}'
 unset ARGO_NEW_PW
 unset ARGO_ADMIN_PW
-kubectl apply -f https://raw.githubusercontent.com/natereid72/gitops/main/main/main.yaml
